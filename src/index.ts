be867baf32abcdd689aa5fb36c1cc05a6f7169a3
Ben Talbot <b.talbot@qut.edu.au>
2022-03-02 16:13:00 +1000

    Export classes from default entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export function HelloWorld(name: string) {
-  return `Hello ${name}`;
-}
+export {default as BottomBar} from './components/bottom_bar';
+export {default as CardCarousel} from './components/card_carousel';
+export {default as ContentCard} from './components/content_card';
+export {default as FocusButton} from './components/focus_button';
+export {default as Layout} from './components/layout';
+export {default as ResponsiveMedia} from './components/responsive_media';
+export {default as SimpleDialog} from './components/simple_dialog';
+export {default as TopBar} from './components/top_bar';
+
+export * from './styles/shared';
