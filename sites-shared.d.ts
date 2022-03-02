28ef96a4345bd11bd79855eea30dbac4a24eab0f
Ben Talbot <b.talbot@qut.edu.au>
2022-03-02 15:30:27 +1000

    Add missing dependencies for types

--- sites-shared.d.ts
+++ sites-shared.d.ts
@@ -2,3 +2,12 @@ declare module '*.js' {
   const content: React.StatelessComponent<React.SVGAttributes<SVGElement>>;
   export default content;
 }
+
+// Workaround related to: https://github.com/vercel/next.js/issues/29788
+// ... should be able to delete at some point in future ...
+declare type StaticImageData = {
+  src: string;
+  height: number;
+  width: number;
+  placeholder?: string;
+};
