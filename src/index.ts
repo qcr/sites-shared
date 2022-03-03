f88d0f86ecf063bcc734754e5288844043f85abc
Ben Talbot <b.talbot@qut.edu.au>
2022-03-03 14:21:16 +1000

    Remove site-specific layout component

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ export {
   ContentCardProps,
 } from './components/content_card';
 export {default as FocusButton} from './components/focus_button';
-export {default as Layout} from './components/layout';
 export {default as ResponsiveMedia} from './components/responsive_media';
 export {default as SimpleDialog} from './components/simple_dialog';
 export {default as TopBar} from './components/top_bar';
