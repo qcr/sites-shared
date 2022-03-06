22d71240c4aa864d6cfae676e9e39794e790d10e
Ben Talbot <b.talbot@qut.edu.au>
2022-03-06 17:03:42 +1000

    Add NextJs example to library

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type {NextPage} from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <h3>Test</h3>
+      <div>Hello?</div>
+    </>
+  );
+};
+
+export default Home;
