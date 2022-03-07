0674b964c8d14f43332b1f9fd31705f752e36b94
Ben Talbot <b.talbot@qut.edu.au>
2022-03-07 10:53:45 +1000

    Flesh out demo page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,61 @@
-import {QcrBody, QcrBottomBar, QcrPage, QcrTitle, QcrTopBar} from '../src';
+import FocusButton from '../lib/components/focus_button';
+import {
+  QcrBody,
+  QcrBottomBar,
+  QcrPage,
+  QcrText,
+  QcrTitle,
+  QcrTopBar,
+} from '../src';
 
 export default function HomePage() {
   return (
     <QcrPage>
-      <QcrTopBar title="Dummy page" />
-      <QcrTitle variant="h1">QCR Title</QcrTitle>
+      <QcrTopBar
+        title="Sample homepage"
+        tabs={[
+          {text: 'Home', target: '/'},
+          {text: 'External', target: 'https://research.qut.edu.au/qcr'},
+        ]}
+        selected={0}
+      />
+      <QcrTitle variant="h3" color="primary">
+        QCR title
+      </QcrTitle>
       <QcrBody>
-        <p>working</p>
+        <QcrText>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+            commodi officiis aperiam odit qui eius reiciendis ipsum laborum
+            provident accusantium cum, sint facere aspernatur repellat a ad
+            atque repellendus consequatur.
+          </p>
+          <QcrTitle variant="h4" color="primary">
+            Some cards
+          </QcrTitle>
+          <QcrTitle variant="h5" color="primary">
+            Feature cards
+          </QcrTitle>
+          TODO
+          <QcrTitle variant="h5" color="primary">
+            Content cards
+          </QcrTitle>
+          <QcrTitle variant="h5" color="primary">
+            A carousel of content cards
+          </QcrTitle>
+          <QcrTitle variant="h4" color="primary">
+            Some buttons and dialogs
+          </QcrTitle>
+          <QcrTitle variant="h4" color="primary">
+            Responsive media
+          </QcrTitle>
+          This supports multiple media sources, and handles both images and
+          videos. Good for web optimisation where you have to balance supporting
+          both highly-optimised and widely-supported media.
+          <QcrTitle variant="h4" color="primary">
+            Some placeholders
+          </QcrTitle>
+        </QcrText>
       </QcrBody>
       <QcrBottomBar />
     </QcrPage>
