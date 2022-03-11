63325841319e48b6fdc752f36d5d95e967b4e8c9
Ben Talbot <b.talbot@qut.edu.au>
2022-03-11 17:31:08 +1000

    Add basic markdown loader using markdown-it

---
+++ src/loaders/markdown.ts
@@ -0,0 +1,31 @@
+import mdi from 'markdown-it';
+
+import type * as webpack from 'webpack';
+
+const renderer = mdi({
+  html: true,
+})
+  .use(require('markdown-it-block-embed'), {
+    containerClassName: 'embedded-block',
+  })
+  .use(require('markdown-it-prism'));
+
+async function asyncLoader(
+  ctx: webpack.LoaderContext<any>,
+  input: string,
+  cb: (err: Error | null, result: string) => void
+) {
+  const out = renderer.render(input);
+  cb(null, ctx.loaderIndex == 0 ? `export default ${out}` : out);
+}
+
+export default function loader(
+  this: webpack.LoaderContext<any>,
+  input: string
+) {
+  const cb = this.async();
+  asyncLoader(this, input, (e, r) => {
+    if (e) return cb(e);
+    cb(null, r);
+  });
+}
