9e5f011658013f7b96b8f4a1aba40e1336a05771
Ben Talbot <b.talbot@qut.edu.au>
2022-03-18 21:40:16 +1000

    Add rant on the dribble encountered getting here

--- src/loaders/markdown.ts
+++ src/loaders/markdown.ts
@@ -1,6 +1,30 @@
-import {renderToString} from 'react-dom/server';
-
 import type * as webpack from 'webpack';
+
+// Why do we end up with this weird looking rubbish?
+// 1. Next packages some compiled webpack stuff that doesn't support ESM
+// modules (some error with 'next/dist/compiled/webpack/bundle5.js')
+// 2. MDX and Remark have moved onto only ESM support
+//      https://gist.github.com/sindresorhus/a39789f98801d908bbc7ff3ecc99d99c
+// 3. Even though Next Config files support ESM modules, the bundled webpack
+// errors stop you using them...
+//      https://github.com/vercel/next.js/issues/9607
+// 4. We can't use '@mdx-js/loader' as we have no way of setting options (Next
+// Config can't use ESM modules and loaders simultaneously, and there's no
+// loader syntax for passing options objects in code)
+//      https://webpack.js.org/api/loaders/
+// 5. We can't use the "dynamic import()" suggestion because Typescript doesn't
+// have a mode to output "commonjs with dynamic imports" (it replaces them all
+// with require() calls...)
+//      https://github.com/microsoft/TypeScript/issues/43329
+// 6. Bringing in the nightly build with 'module: "node12"' results in other
+// errors further down the chain...
+//
+// So we end up with the little hacks below from this comment:
+//      https://github.com/microsoft/TypeScript/issues/43329#issuecomment-1008361973
+//
+// Perfect example of how shit an ecosystem JS is to work with. In what world
+// is a developer going to enjoy going through that mess just to get expected
+// language functionality working?
 
 const mdx = Function('return import("@mdx-js/mdx")')() as Promise<
   typeof import('@mdx-js/mdx')
