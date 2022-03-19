e14c39439c0d38ebc64c12200626316b7a896881
Ben Talbot <b.talbot@qut.edu.au>
2022-03-19 10:55:55 +1000

    Remove noise logging in handlebars helpers

--- src/loaders/handlebars-helpers.tsx
+++ src/loaders/handlebars-helpers.tsx
@@ -3,7 +3,7 @@ import Handlebars, {HelperDeclareSpec} from 'handlebars';
 const helpers: HelperDeclareSpec = {
   list: (ctx: string) => {
     const x = new Handlebars.SafeString('<emph>Emphasised text</emph>');
-    console.log(x);
+    /* console.log(x); */
     return x;
   },
   /* list: (ctx: string) => 'hello', */
